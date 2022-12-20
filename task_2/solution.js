21cda19afde9c336f1cb23a2cdb073a7652e9ebc
Regina Evstafyeva <muuurrr@inbox.ru>
2022-12-20 20:50:56 +0300

    task_2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -16,7 +16,20 @@ function calcShipping(sum, min, shipping) {
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
    
-   
+    let shippingSum;
+    if (productsSum === 0)
+    {
+        shippingSum = 0;
+    }
+    if (productsSum >= freeShippingMinSum)
+    {
+        shippingSum = 0;
+    }
+    if (productsSum > 0 & productsSum < freeShippingMinSum)
+    {
+        shippingSum = shippingPrice;
+    }
+
     // Конец решения задания №2.1.
 
     return shippingSum;
@@ -35,7 +48,12 @@ function calcDiscount(sum, min, discount) {
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
 
-    
+    let discountSum;
+    if (productsSum >= discountMinSum)
+    {
+    discountSum = discountPart / 100 * productsSum;
+    else discountSum = 0;
+    }
 
     // Конец решения задания №2.2.
 
