097a13cc654fb3366d6adb11b739a7a687a7acb2
Regina Evstafyeva <muuurrr@inbox.ru>
2022-12-20 22:51:23 +0300

    task_2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -73,7 +73,7 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // уменьшите totalSum на discountSum
     let totalSum;
     totalSum = productsSum;
-    totalSum -= totalSum * discountSum / 100;
+    totalSum -= discountSum;
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
     // прибавьте к totalSum значение shippingSum
